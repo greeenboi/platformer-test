1d58dd4a8a61a16961fc66aee41a3fbf75b2ec9b
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-12-17 01:42:51 +0530

    Added Player animations and more enemies and better movement

--- scripts/kill_zone.gd
+++ scripts/kill_zone.gd
@@ -4,8 +4,11 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died bruh!!")
+	Engine.time_scale = 0.4
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
